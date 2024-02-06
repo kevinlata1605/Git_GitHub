b7fa6616f8ff6e38bcfb46810702f7b84a0c10a7
cess15 <cesarlata1@hotmail.com>
2024-02-06 18:45:26 -0500

    Se agrego titulo a la pagina web

--- include/index.php
+++ include/index.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pagina Web Include</title>
 </head>
 
 <body>
-    <?php include_once "./inc/nav.php";?>
     <h1>PAGINA PRINCIPAL</h1>
-    <?php include_once "./inc/footer.php";?>
+    <?php include_once "./inc/nav.php"; ?>
+    <?php include_once "./inc/footer.php"; ?>
 </body>
 
 </html>
