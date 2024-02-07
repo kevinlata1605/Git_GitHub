6433edd02593aaad62fdc4fb4cfa07cca2d10316
kevinlata1605 <kevinlata160599@gmail.com>
2024-02-07 17:02:20 -0500

    Se cambio el titulo dela pagina web (Rama Principal Include)

--- include/index.php
+++ include/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Web Include</title>
+    <title>Rama Principal Include</title>
 </head>
 
 <body>
