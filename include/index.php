2278beca5ed20acd56681b36dd71d835f8f34bbf
Kevin Lata Jácome <159205031+kevinlata1605@users.noreply.github.com>
2024-02-08 10:43:49 -0500

    Se cambio el titulo de la pagina Include
    Prueba de cambios en GitHub

--- include/index.php
+++ include/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rama Principal Include</title>
+    <title>Pagina Principal Include</title>
 </head>
 
 <body>
