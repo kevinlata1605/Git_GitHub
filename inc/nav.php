efdab86a5f305b4572973ec8f738aeb35d6c79be
kevinlata1605 <kevinlata160599@gmail.com>
2024-02-27 10:04:30 -0500

    Se corrigio el NAV, al dar clic a Tercera se dirige a Pagina 3 fixes #1

--- inc/nav.php
+++ inc/nav.php
@@ -2,6 +2,6 @@
         <ul>
         <li> <a href="index.php">Inicio</a></li>
         <li> <a href="index2.php">Secundaria</a></li>
-        <li> <a href="index.php">Tercera</a></li>
+        <li> <a href="index3.php">Tercera</a></li>
         </ul>
     </nav>
